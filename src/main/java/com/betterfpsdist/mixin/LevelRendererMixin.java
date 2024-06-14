7dd02f4e0ec1fe7353ba1de41108faba482557dc
CommandGenius <rvlcariati@gmail.com>
2024-06-14 11:23:12 -0400

    update to 1.21

--- src/main/java/com/betterfpsdist/mixin/LevelRendererMixin.java
+++ src/main/java/com/betterfpsdist/mixin/LevelRendererMixin.java
@@ -1,40 +1,40 @@
 package com.betterfpsdist.mixin;
 
 import com.betterfpsdist.BetterfpsdistMod;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.chunk.ChunkBuilder;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(LevelRenderer.class)
+@Mixin(WorldRenderer.class)
 public class LevelRendererMixin
 {
     @Shadow
     @Final
-    private Minecraft                         minecraft;
-    private ChunkRenderDispatcher.RenderChunk current = null;
+    private MinecraftClient                         minecraft;
+    private ChunkBuilder.BuiltChunk current = null;
     //private HashSet<BlockPos>                 renderedPositions = new HashSet<>();
     //private long                              nextUpdate        = 0;
 
     @Redirect(method = "renderChunkLayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk;getCompiledChunk()Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$CompiledChunk;"))
-    public ChunkRenderDispatcher.CompiledChunk on(final ChunkRenderDispatcher.RenderChunk instance)
+    public ChunkBuilder.ChunkData on(final ChunkBuilder.BuiltChunk instance)
     {
         current = instance;
-        return instance.getCompiledChunk();
+        return instance.getData();
     }
 
     @Redirect(method = "renderChunkLayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher$CompiledChunk;isEmpty(Lnet/minecraft/client/renderer/RenderType;)Z"))
-    public boolean on(final ChunkRenderDispatcher.CompiledChunk instance, final RenderType type)
+    public boolean on(final ChunkBuilder.ChunkData instance, final RenderLayer type)
     {
         boolean returnv =
-          minecraft.cameraEntity != null && distSqr(minecraft.cameraEntity.position(), new Vec3(current.getOrigin().getX(), current.getOrigin().getY(), current.getOrigin().getZ()))
-                                              > (Minecraft.getInstance().options.renderDistance().get() * 16) * (Minecraft.getInstance().options.renderDistance().get() * 16)
+          minecraft.cameraEntity != null && distSqr(minecraft.cameraEntity.getPos(), new Vec3d(current.getOrigin().getX(), current.getOrigin().getY(), current.getOrigin().getZ()))
+                                              > (MinecraftClient.getInstance().options.getViewDistance().getValue() * 16) * (MinecraftClient.getInstance().options.getViewDistance().getValue() * 16)
             || instance.isEmpty(type);
 
 /*
@@ -53,7 +53,7 @@ public class LevelRendererMixin
         return returnv;
     }
 
-    private double distSqr(Vec3 from, Vec3 to)
+    private double distSqr(Vec3d from, Vec3d to)
     {
         double d0 = from.x - to.x;
         double d1 = from.y - to.y;
--- src/main/java/com/betterfpsdist/mixin/VideoSettingsScreenMixin.java
+++ src/main/java/com/betterfpsdist/mixin/VideoSettingsScreenMixin.java
@@ -1,23 +1,23 @@
 package com.betterfpsdist.mixin;
 
 import com.betterfpsdist.event.ClientEventHandler;
-import net.minecraft.client.gui.components.OptionsList;
-import net.minecraft.client.gui.screens.VideoSettingsScreen;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
+import net.minecraft.client.gui.widget.OptionListWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(VideoSettingsScreen.class)
+@Mixin(VideoOptionsScreen.class)
 public class VideoSettingsScreenMixin
 {
     @Shadow
-    private OptionsList list;
+    private OptionListWidget list;
 
     @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/OptionsList;addSmall([Lnet/minecraft/client/OptionInstance;)V"))
     public void on(final CallbackInfo ci)
     {
-        list.addBig(ClientEventHandler.chunkrenderdist);
+        list.addSingleOptionEntry(ClientEventHandler.chunkrenderdist);
     }
 }
